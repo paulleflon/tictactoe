67ec595d854e1d2992dbf1543367bd8fa0826d59
Paul Leflon <leflon.paul@gmail.com>
2025-01-15 20:06:54 +0100

    fix(shared): fix deepUpdate function not updating for null values

--- shared/lib.ts
+++ shared/lib.ts
@@ -1,7 +1,7 @@
 export function deepUpdate(obj?: { [key: string]: any } | null, updates?: { [key: string]: any } | null): void {
 	if (!obj || !updates) return;
 	for (const key of Object.keys(updates)) {
-		if (typeof updates[key] === 'object' && !Array.isArray(updates[key])) {
+		if (updates[key] !== null && typeof updates[key] === 'object' && !Array.isArray(updates[key])) {
 			if (!obj[key]) {
 				obj[key] = {};
 			}
