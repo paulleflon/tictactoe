0b6f851f1cd1e6207452eec27fd87979ac8c54cc
Paul Leflon <leflon.paul@gmail.com>
2025-01-16 00:11:24 +0100

    feat(client): add win animation

--- client/src/components/Grid.vue
+++ client/src/components/Grid.vue
@@ -2,51 +2,53 @@
 import coloredTeam from '@/lib/coloredTeam';
 import socket from '@/lib/socket';
 import { store } from '@/lib/store';
+import { winningConfiguration } from '@/lib/win';
 import type { Grid, GridVoteCounts } from '@/shared/types/Grid';
 import type Status from '@/shared/types/Status';
-import { ref, Transition, watch } from 'vue';
+import { computed, ref, Transition, watch } from 'vue';
 const props = defineProps<{
 	cells: Grid,
 	votes: GridVoteCounts,
 	status: Status
 }>();
-const hasVoted = ref(false);
 
 const vote = (cell: number) => {
-	if (!hasVoted.value && props.status.type === 'vote' && props.status.actor === store.team && props.cells[cell] === null) {
+	if (props.status.type === 'vote' && props.status.actor === store.team && props.cells[cell] === null) {
 		socket.emit('vote', cell);
-		hasVoted.value = true;
 	}
 };
-watch(() => props.status, () => {
-	hasVoted.value = false;
-}, { deep: true });
+
+const winIndicatorClass = computed(() => props.status.type === 'win' ? `win-indicator ${props.status.actor} ${winningConfiguration(props.cells)}` : '');
 </script>
 
 <template>
-	<table>
-		<tbody>
-			<tr v-for='i in 3'>
-				<td v-for='j in 3' @click='vote(3 * (i - 1) + j - 1)'>
-					<Transition name='new-cell'>
-						<div v-if='props.cells[3 * (i - 1) + j - 1] !== null'
-							v-html='coloredTeam(props.cells[3 * (i - 1) + j - 1])'>
-						</div>
-					</Transition>
-					<Transition name='vote-cell'>
-						<div class='voting-cell' v-if='status.type === "vote" && props.votes[3 * (i - 1) + j - 1] > 0'>
-							<span>
-								{{ props.status.actor }}
-							</span>
-							<div class='vote-count'>
-								{{ props.votes[3 * (i - 1) + j - 1] }}
+	<div class='grid'>
+		<div :class='winIndicatorClass' v-if='true || props.status.type === "win"'></div>
+		<table>
+			<tbody>
+				<tr v-for='i in 3'>
+					<td v-for='j in 3' @click='vote(3 * (i - 1) + j - 1)'>
+						<Transition name='new-cell'>
+							<div v-if='props.cells[3 * (i - 1) + j - 1] !== null'
+								v-html='coloredTeam(props.cells[3 * (i - 1) + j - 1])'>
 							</div>
-						</div>
-					</Transition>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+						</Transition>
+						<Transition name='vote-cell'>
+							<div class='voting-cell'
+								v-if='status.type === "vote" && props.votes[3 * (i - 1) + j - 1] > 0'>
+								<span>
+									{{ props.status.actor }}
+								</span>
+								<div class='vote-count'>
+									{{ props.votes[3 * (i - 1) + j - 1] }}
+								</div>
+							</div>
+						</Transition>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
 
 <style scoped>
@@ -74,15 +76,181 @@ watch(() => props.status, () => {
 	filter: blur(10px);
 }
 
-table {
+.grid {
 	position: relative;
-	border: 1px solid black;
 	width: 400px;
 	height: 400px;
 	max-width: 90vw;
 	max-height: 90vw;
-	border-collapse: collapse;
 	margin: 0 auto;
+}
+
+.win-indicator {
+	position: absolute;
+	top: 82.5%;
+	border-radius: 10em;
+	z-index: 1;
+	animation-duration: 3333ms;
+	animation-timing-function: ease;
+	animation-iteration-count: infinite;
+
+	&.x {
+		background: var(--team-x-color);
+	}
+
+	&.o {
+		background: var(--team-o-color);
+	}
+
+	&.top,
+	&.middle,
+	&.bottom {
+		animation-name: strike-h;
+		height: 12px;
+		transform: translateY(-50%);
+	}
+
+	&.top {
+		top: 16.5%;
+	}
+
+	&.middle {
+		top: 49.5%;
+	}
+
+	&.bottom {
+		top: 83%;
+	}
+
+	&.left,
+	&.center,
+	&.right {
+		animation-name: strike-v;
+		width: 12px;
+		transform: translateX(-50%);
+	}
+
+	&.left {
+		left: 16.5%;
+	}
+
+	&.center {
+		left: 49.5%;
+	}
+
+	&.right {
+		left: 83.5%;
+	}
+
+	&.top-left {
+		animation-name: strike-tl;
+		height: 12px;
+		transform-origin: top left;
+		transform: rotate(45deg);
+	}
+
+	&.top-right {
+		animation-name: strike-tr;
+		height: 12px;
+		transform-origin: top right;
+		transform: rotate(-45deg);
+	}
+
+}
+
+@keyframes strike-tr {
+
+	0%,
+	49% {
+		top: calc(1% * 1.414);
+		right: calc(2.5% * 1.414);
+		bottom: auto;
+		left: auto;
+	}
+
+	50% {
+		width: calc(95% * 1.414);
+		bottom: auto;
+		left: auto;
+		transform-origin: top right;
+	}
+
+	50.0001%,
+	100% {
+		transform-origin: bottom left;
+		top: auto;
+		right: auto;
+		bottom: calc(1% * 1.414);
+		left: calc(2.5% * 1.414);
+	}
+}
+
+@keyframes strike-tl {
+
+	0%,
+	49% {
+		top: calc(1% * 1.414);
+		left: calc(2.5% * 1.414);
+		transform-origin: top left;
+	}
+
+	50% {
+		width: calc(95% * 1.414);
+		transform-origin: top left;
+	}
+
+	50%,
+	100% {
+		transform-origin: bottom right;
+		top: auto;
+		left: auto;
+		bottom: calc(1% * 1.414);
+		right: calc(2.5% * 1.414);
+	}
+}
+
+@keyframes strike-h {
+
+	0%,
+	49% {
+		left: 2.5%;
+	}
+
+	50% {
+		width: 95%;
+	}
+
+	51%,
+	100% {
+		left: auto;
+		right: 2.5%;
+	}
+}
+
+@keyframes strike-v {
+
+	0%,
+	49% {
+		top: 2.5%;
+	}
+
+	50% {
+		height: 95%;
+	}
+
+	51%,
+	100% {
+		top: auto;
+		bottom: 2.5%;
+	}
+}
+
+table {
+	position: relative;
+	border: 1px solid black;
+	border-collapse: collapse;
+	width: 100%;
+	height: 100%;
 }
 
 td {
@@ -100,6 +268,7 @@ td {
 		background-color: #f0f0f0;
 	}
 }
+
 .voting-cell {
 	position: absolute;
 	top: 0;
@@ -111,6 +280,7 @@ td {
 	width: 100%;
 	height: 100%;
 }
+
 .vote-count {
 	position: absolute;
 	bottom: 0;
---
+++ client/src/lib/win.ts
@@ -0,0 +1,46 @@
+import type { Grid } from '@/shared/types/Grid';
+
+type WinningConfiguration =
+	| 'top'
+	| 'middle'
+	| 'bottom'
+	| 'left'
+	| 'center'
+	| 'right'
+	| 'top-left'
+	| 'top-right';
+export type { WinningConfiguration };
+
+export function winningConfiguration(grid: Grid): WinningConfiguration | null {
+	const winningConfigurations = [
+		// horizontal
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		// vertical
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		// diagonal
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	const configNames: WinningConfiguration[] = [
+		'top',
+		'middle',
+		'bottom',
+		'left',
+		'center',
+		'right',
+		'top-left',
+		'top-right',
+	];
+
+	for (const configuration of winningConfigurations) {
+		const [a, b, c] = configuration;
+		if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+			return configNames[winningConfigurations.indexOf(configuration)];
+		}
+	}
+	return null;
+}
