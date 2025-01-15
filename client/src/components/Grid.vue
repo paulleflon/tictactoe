8ce731a582a9b5cdc509799d37ea7e6f2b3dcd0a
Paul Leflon <leflon.paul@gmail.com>
2025-01-15 23:11:42 +0100

    feat(client): add grid animations

--- client/src/components/Grid.vue
+++ client/src/components/Grid.vue
@@ -1,10 +1,10 @@
 <script setup lang='ts'>
 import coloredTeam from '@/lib/coloredTeam';
+import socket from '@/lib/socket';
+import { store } from '@/lib/store';
 import type { Grid, GridVoteCounts } from '@/shared/types/Grid';
 import type Status from '@/shared/types/Status';
-import { store } from '@/lib/store';
-import socket from '@/lib/socket';
-import { ref, watch } from 'vue';
+import { ref, Transition, watch } from 'vue';
 const props = defineProps<{
 	cells: Grid,
 	votes: GridVoteCounts,
@@ -28,17 +28,21 @@ watch(() => props.status, () => {
 		<tbody>
 			<tr v-for='i in 3'>
 				<td v-for='j in 3' @click='vote(3 * (i - 1) + j - 1)'>
-					<div v-if='props.cells[3 * (i - 1) + j - 1] !== null'
-						v-html='coloredTeam(props.cells[3 * (i - 1) + j - 1])'>
-					</div>
-					<div class='voting-cell' v-else-if='status.type === "vote" && props.votes[3 * (i - 1) + j - 1] > 0'>
-						<span>
-							{{ props.status.actor }}
-						</span>
-						<div class='vote-count'>
-							{{ props.votes[3 * (i - 1) + j - 1] }}
+					<Transition name='new-cell'>
+						<div v-if='props.cells[3 * (i - 1) + j - 1] !== null'
+							v-html='coloredTeam(props.cells[3 * (i - 1) + j - 1])'>
 						</div>
-					</div>
+					</Transition>
+					<Transition name='vote-cell'>
+						<div class='voting-cell' v-if='status.type === "vote" && props.votes[3 * (i - 1) + j - 1] > 0'>
+							<span>
+								{{ props.status.actor }}
+							</span>
+							<div class='vote-count'>
+								{{ props.votes[3 * (i - 1) + j - 1] }}
+							</div>
+						</div>
+					</Transition>
 				</td>
 			</tr>
 		</tbody>
@@ -46,6 +50,30 @@ watch(() => props.status, () => {
 </template>
 
 <style scoped>
+.new-cell-enter-active {
+	transition: transform 0.5s cubic-bezier(0.68, -0.55, 0.27, 2.55);
+}
+
+.new-cell-leave-active {
+	transition: transform .8s ease;
+}
+
+.new-cell-enter-from,
+.new-cell-leave-to {
+	transform: scale(0);
+}
+
+.vote-cell-enter-active,
+.vote-cell-leave-active {
+	transition: all .15s ease;
+}
+
+.vote-cell-enter-from,
+.vote-cell-leave-to {
+	opacity: 0;
+	filter: blur(10px);
+}
+
 table {
 	position: relative;
 	border: 1px solid black;
@@ -72,7 +100,17 @@ td {
 		background-color: #f0f0f0;
 	}
 }
-
+.voting-cell {
+	position: absolute;
+	top: 0;
+	left: 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+}
 .vote-count {
 	position: absolute;
 	bottom: 0;
