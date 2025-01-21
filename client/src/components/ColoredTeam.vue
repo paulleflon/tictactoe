57db3bf16f2e89b4e9b88bc564f2da8a01d39cd1
Paul Leflon <leflon.paul@gmail.com>
2025-01-21 12:03:07 +0100

    fix(client): replace unsafe coloredTeam function with component

---
+++ client/src/components/ColoredTeam.vue
@@ -0,0 +1,23 @@
+<script lang='ts' setup>
+import type { CellValue } from '@/shared/types/Grid';
+import { computed } from 'vue';
+
+const props = defineProps<{
+	team: CellValue;
+}>();
+const color = computed(() => {
+	if (props.team)
+		return { x: 'var(--team-x-color)', o: 'var(--team-o-color)' }[props.team]
+	else
+		return '';
+});
+</script>
+<template>
+	<span :style='{ color: color }'>{{ props.team || '' }}</span>
+</template>
+
+<style scoped>
+span {
+	text-transform: uppercase;
+}
+</style>
