fa05a0bf380c2aaa842fc55974e1edb702228837
Paul Leflon <leflon.paul@gmail.com>
2025-01-21 12:04:41 +0100

    fix(client): prevent localStorage forbidden manipulations

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,7 +1,12 @@
 import type { CellValue } from '@/shared/types/Grid';
 import type { IGame } from '@/shared/types/IGame';
 import { reactive } from 'vue';
-const savedTeam = localStorage.getItem('3t_team') as 'x' | 'o' | null;
+let savedTeam = localStorage.getItem('3t_team') as 'x' | 'o' | null;
+if (savedTeam !== 'x' && savedTeam !== 'o') {
+	savedTeam = null;
+	localStorage.removeItem('3t_team');
+}
+
 export const store = reactive({
 	team: savedTeam,
 	game: null as IGame | null,
